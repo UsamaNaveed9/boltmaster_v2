9f095b10a35fcfd64f2dd43ffd234a45ce480900
UsamaNaveed9 <usamanaveed9263@gmail.com>
2022-02-22 07:31:54 +0000

    add sales invoice item

--- boltmaster_v2/boltmaster_v2/report/item_ledger_details/item_ledger_details.py
+++ boltmaster_v2/boltmaster_v2/report/item_ledger_details/item_ledger_details.py
@@ -173,7 +173,7 @@ def get_items(filters):
 			`tabSales Invoice`.customer_name
 		from `tabSales Invoice`, `tabSales Invoice Item`
 		where `tabSales Invoice`.name = `tabSales Invoice Item`.parent
-			and `tabSales Invoice`.docstatus = 1 and `tabSales Invoice`.item_code = `tabStock Ledger Entry`.item_code
+			and `tabSales Invoice`.docstatus = 1 and `tabSales Invoice Item`.item_code = `tabStock Ledger Entry`.item_code
 		""".format(sle_conditions=get_sle_conditions(filters)),
 		filters, as_dict=1) #nosec		
 	return items, row
