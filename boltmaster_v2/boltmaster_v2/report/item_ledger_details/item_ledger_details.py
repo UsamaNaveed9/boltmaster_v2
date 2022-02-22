3cdbfcd1767f21372176306d8cde3120bda65640
UsamaNaveed9 <usamanaveed9263@gmail.com>
2022-02-22 06:19:42 +0000

    add columns

--- boltmaster_v2/boltmaster_v2/report/item_ledger_details/item_ledger_details.py
+++ boltmaster_v2/boltmaster_v2/report/item_ledger_details/item_ledger_details.py
@@ -2,6 +2,7 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
+from turtle import width
 
 import frappe
 from frappe import _
@@ -101,7 +102,10 @@ def get_columns():
 		{"label": _("Incoming Rate"), "fieldname": "incoming_rate", "fieldtype": "Currency", "width": 110, "options": "Company:company:default_currency", "convertible": "rate"},
 		{"label": _("Valuation Rate"), "fieldname": "valuation_rate", "fieldtype": "Currency", "width": 110, "options": "Company:company:default_currency", "convertible": "rate"},
 		{"label": _("Balance Value"), "fieldname": "stock_value", "fieldtype": "Currency", "width": 110, "options": "Company:company:default_currency"},
-		{"label": _("Voucher Type"), "fieldname": "voucher_type", "width": 110}
+		{"label": _("Voucher Type"), "fieldname": "voucher_type", "width": 110},
+		{"label": _("Customer"), "fieldname": "customer", "fieldtype": "Link", "options": "Customer", "width": 110},
+		{"label": _("Rate"), "fieldname": "rate", "width": 110},
+		{"lable": _("Amount"), "fieldname": "amount", "width": 110}
 	]
 
 	return columns
