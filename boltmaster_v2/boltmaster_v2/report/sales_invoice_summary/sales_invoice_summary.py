2e54321adafd15ba4b306461a3098db75a69fccd
UsamaNaveed9 <usamanaveed9263@gmail.com>
2022-03-05 10:54:10 +0000

    add if condition

--- boltmaster_v2/boltmaster_v2/report/sales_invoice_summary/sales_invoice_summary.py
+++ boltmaster_v2/boltmaster_v2/report/sales_invoice_summary/sales_invoice_summary.py
@@ -75,8 +75,9 @@ def _execute(filters, additional_table_columns=None, additional_query_columns=No
 		#multiple delivery notes
 		note = row['delivery_note']
 		for d in frappe.db.get_list("Sales Invoice Item", filters={"parent": inv.name}, fields=["delivery_note"]):
-			if row['delivery_note'] != d.delivery_note:
-				note = note + ", " + str(d.delivery_note)
+			if d.delivery_note:
+				if row['delivery_note'] != d.delivery_note:
+					note = note + ", " + str(d.delivery_note)
 
 		row.update({
 			'delivery_note': note
